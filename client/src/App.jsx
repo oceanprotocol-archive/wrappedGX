f3013723d6578092e5828e6edb9054f1f824867d
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-06-18 15:29:05 +0300

    using react hooks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,38 @@
-import React, { Component } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import getWeb3 from "./utils/getWeb3";
 import getContract from "./utils/getContract";
 
 import "./App.css";
 
-class App extends Component {
-  state = { web3: null, accounts: null, contract: null };
+export default function App(){
+  const [web3, updateWeb3] = useState()
+  const [account, updateAccount] = useState()
+  const [contract, updateContract] = useState()
+  const [ballanceGX, updateBallanceGX] = useState()
+  const [ballanceWGX, updateBallanceWGX] = useState()
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-      const instance = await getContract(web3)
-       // Use web3 to get the user's accounts.
-       const accounts = await web3.eth.getAccounts();
+  useEffect(() => {
+    const getData = async () => {
+    const web3 = await getWeb3();
+    const instance = await getContract(web3)
+    const account = await web3.eth.getAccounts();
 
-      // Set web3, accounts, and contract to the state
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+    updateWeb3(web3)
+    updateContract(instance)
+    updateAccount(account)
     }
-  };
+    getData()
+  })
 
-
-
-  render() {
-    if (!this.state.web3) {
+    if (!web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
         <h1>Exchange GX for WrappedGX (WGX)</h1>
-        <p>Your account: {this.state.accounts}</p>
+        <p>Your account: {account}</p>
 
       </div>
     );
-  }
+  
 }
-
-export default App;
