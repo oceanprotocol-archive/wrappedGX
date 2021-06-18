4b95fca7b40f7c43ea5c5342cef1609dba9de140
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-06-18 14:24:16 +0300

    loading contract instance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,21 @@
 import React, { Component } from "react";
-import WrappedGX from "./contracts/WrappedGX.json";
 import getWeb3 from "./utils/getWeb3";
+import getContract from "./utils/getContract";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
+      const instance = await getContract(web3)
+       // Use web3 to get the user's accounts.
+       const accounts = await web3.eth.getAccounts();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = WrappedGX.networks[networkId];
-      const instance = new web3.eth.Contract(
-        WrappedGX.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
+      // Set web3, accounts, and contract to the state
       this.setState({ web3, accounts, contract: instance }, this.runExample);
     } catch (error) {
       // Catch any errors for any of the above operations.
@@ -44,7 +35,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Exchange GX for WrappedGX (WGX)</h1>
-       
+        <p>Your account: {this.state.accounts}</p>
+
       </div>
     );
   }
---
+++ client/src/utils/getContract.js
@@ -0,0 +1,18 @@
+import WrappedGX from "../contracts/WrappedGX.json";
+
+export default async function getContract(web3){  
+      let instance = null
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = WrappedGX.networks[networkId];
+      
+      if (deployedNetwork){
+        instance = new web3.eth.Contract(
+            WrappedGX.abi,
+            deployedNetwork && deployedNetwork.address,
+        );
+      } else{
+          window.alert("Sorry, the WrappedGX smart contract is not deployed to the current network.")
+      }
+      return instance
+}
