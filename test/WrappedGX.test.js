24bff9c7682d59ca9efdc2f5416c4921090e3459
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-06-18 11:04:57 +0300

    Creating function tests

--- test/WrappedGX.test.js
+++ test/WrappedGX.test.js
@@ -1,6 +1,6 @@
 const wrappedGX = artifacts.require("WrappedGX");
 const { assert } = require('chai');
-
+const { BN, expectRevert } = require('@openzeppelin/test-helpers')
 const BigNumber = web3.BigNumber;
 
 require('chai')
@@ -8,7 +8,7 @@ require('chai')
     .should()
 
     contract('wrappedGX', accounts => {
-    
+        const [deployer, alice, bob, charlie] = accounts
         let WGX;
     
         before(async () => {
@@ -28,7 +28,7 @@ require('chai')
         })
     
         beforeEach(async function() {
-            this.WGX = await wrappedGX.new()
+            this.WGX = await wrappedGX.new({ from: deployer })
         })
     
         describe('Token attributes', function(){
@@ -44,9 +44,15 @@ require('chai')
                 const decimals = (await this.WGX.decimals()).toNumber();
                 decimals.should.be.bignumber.equal(18);
             });
-            it('Has the correct initial balance', async function(){
-                const balance = (await this.WGX.balanceOf('0xCC97Ed04b5b8912694a9254205ad5b9316755fF0')).toNumber();
-                balance.should.be.bignumber.equal(0);
+            it('Accounts have the correct initial balance', async function(){
+                const balanceDeployer = (await this.WGX.balanceOf(deployer)).toNumber();
+                const balanceAlice = (await this.WGX.balanceOf(alice)).toNumber();
+                const balanceBob = (await this.WGX.balanceOf(bob)).toNumber();
+                const balanceCharlie = (await this.WGX.balanceOf(charlie)).toNumber();
+                balanceDeployer.should.be.bignumber.equal(0);
+                balanceAlice.should.be.bignumber.equal(0);
+                balanceBob.should.be.bignumber.equal(0);
+                balanceCharlie.should.be.bignumber.equal(0);
             });
             it('Has the correct initial total supply', async function(){
                 const balance = (await this.WGX.totalSupply()).toNumber();
@@ -55,9 +61,18 @@ require('chai')
         })
 
         describe('Contract functions', function(){
-            it('Has the correct name', async function(){
-                const name = await this.WGX.name();
-                name.should.equal("Wrapped GX");
+            it('Emits deposit event', async function(){
+                const deposit = await this.WGX.deposit();
+                assert.notEqual(deposit.tx, 0x0);
+                assert.notEqual(deposit.tx, '');
+                assert.notEqual(deposit.tx, null);
+                assert.notEqual(deposit.tx, undefined);
+            });
+            it('Alice deposits GX', async function(){
+                const balanceBefore = await WGX.balanceOf(alice)
+                await WGX.sendTransaction({ from: alice, value: 1 })
+                const balanceAfter = await WGX.balanceOf(alice)
+                balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
             });
         })
     })
