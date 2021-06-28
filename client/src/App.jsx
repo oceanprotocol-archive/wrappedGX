97799b020b70f5df6983108b0939fe80f76f5a7e
Jamie Hewitt <jamie.hewitt15@gmail.com>
2021-06-28 11:53:08 +0300

    formatting code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Exchange from "./components/Exchange";
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Exchange from './components/Exchange'
 import './components/exchange.css'
 
-import "./App.css";
+import './App.css'
 
-export default function App(){
-
- 
-    return (
-      <div className="App">
-        <h1>Exchange GX for WrappedGX (WGX)</h1>
-        <Exchange />
-      </div>
-    );
-  
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Exchange GX for WrappedGX (WGX)</h1>
+      <Exchange />
+    </div>
+  )
 }
